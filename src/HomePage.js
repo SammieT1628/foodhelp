e6c063dbc7755f3a27c0c5e179801fe09771cb44
Samantha Thompson <samanthathompson@Samanthas-Mini.lan>
2024-02-07 23:14:12 -0600

    doing layout for routing

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+            <p>Hello this is home page.</p>
+        </div>
+    )
+  }
+}
+
+export default Home;
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+        <div>
+            <p>Nav Bar</p>
+        </div>
+    )
+  }
+}
+
+export default Navbar;
