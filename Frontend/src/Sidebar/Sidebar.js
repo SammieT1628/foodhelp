5ee2f78ed3ce96c6d04402ac67c48c07cc8da93d
Samantha Thompson <samanthathompson@Samanthas-Mini.lan>
2024-02-18 01:22:30 -0600

    added tailwind possibly working properly

---
+++ Frontend/src/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+        <div className='bg-slate-300 w-full h-8'>
+             <h1>Sidebar</h1>
+            
+        </div>
+    )
+  }
+}
+
+export default Sidebar;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/MainArea/*.{js,jsx,ts,tsx}",
+    "./src/Navbar/*.{js,jsx,ts,tsx}",
+    "./src/Sidebar/*.{js,jsx,ts,tsx}",
+    './public/*.html',
   ],
   theme: {
     extend: {},
