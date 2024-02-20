f0e40fbdafe64455e9a128338a342c4b644a6155
Samantha Thompson <samanthathompson@Samanthas-Mini-1.lan>
2024-02-20 16:17:44 -0600

    adding typscript to front end and changing files

---
+++ Frontend/src/MainArea/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+            <h1 className='text-xl'>Hello this is Home page.</h1>
+        </div>
+    )
+  }
+}
+
+export default Home;
