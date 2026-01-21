8038fe262d8329ac96274a4374d5d1734aec8107
Sammie <31319689+SammieT1628@users.noreply.github.com>
2026-01-21 00:42:24 -0600

    setting up recipe model

--- Backend/models/recipe.model.ts
+++ Backend/models/recipe.model.ts
@@ -1,1 +1,17 @@
-import ( Schema, model ) from 'mongoose';
+import { Schema, model } from "mongoose";
+
+const ingredients = new Schema({
+    name: {type: String, required: true},
+    amount: {type: Number, required: true},
+    notes: {type: String},
+})
+
+const instructions = new Schema({
+    step: {type: String, required: true}
+})
+
+const recipe = new Schema({
+    name: { type: String, required: true},
+    ingredients: {type: [ingredients], required: true},
+
+})
